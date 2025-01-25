9da65295e64c311ba9b3efe7b48523f5e0b074ab
Horiatech15 <horiatech15@gmail.com>
2025-01-25 22:58:15 +0100

    update modules

--- bm_pwa_icon/controllers/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# See LICENSE file for full licensing details.
-# See COPYRIGHT file for full copyright details.
-# Developed by Bizmate - Unbox Solutions Co., Ltd.
-
-from . import pwa
--- sh_all_in_one_debranding/sh_backend_debranding/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Softhealer Technologies.
-
-from . import debaranding_config
-# from . import mail_thread
-from . import ir_actions
-from . import ir_model
-from . import ir_ui_view
